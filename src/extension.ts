2339383a63a78a22fbffa3adcd3df7948eaaaacb
Tony Tang <tangchang21@gmail.com>
2023-06-27 23:11:19 +0800

    create extension; add command tctony.switchHeaderSource

---
+++ src/extension.ts
@@ -0,0 +1,25 @@
+import * as vscode from 'vscode';
+
+function registerCommand(context: vscode.ExtensionContext, command: string, callback: (...args: any[]) => any, thisArg?: any): void {
+	const disposable = vscode.commands.registerCommand(command, callback, thisArg);
+	context.subscriptions.push(disposable);
+}
+
+export function activate(context: vscode.ExtensionContext) {
+	console.log('extension "tctony" is now active!');
+
+	registerCommand(context, 'tctony.helloWorld', () => {
+		vscode.window.showInformationMessage('Hello World from tctony!');
+	});
+
+	// clangd启用时，使用clangd的switchheadersource命令
+	registerCommand(context, 'tctony.switchHeaderSource', () => { 
+		if (vscode.extensions.getExtension('llvm-vs-code-extensions.vscode-clangd')?.isActive) {
+			vscode.commands.executeCommand('clangd.switchheadersource');
+		} else {
+			vscode.commands.executeCommand('C_Cpp.SwitchHeaderSource');
+		}
+	});
+}
+
+export function deactivate() {}
